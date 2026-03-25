567b66febc003e7868d17f2dccd14adc5fa5edbf
dohyundev <dohyundev0425@gmail.com>
2026-03-25 20:24:32 +0900

    feat: 사원 커맨드 DTO 추가

--- src/main/kotlin/com/dohyundev/review/employee/domain/dto/RegisterEmployeeCommand.kt
+++ src/main/kotlin/com/dohyundev/review/employee/domain/dto/RegisterEmployeeCommand.kt
@@ -1,3 +1,11 @@
 package com.dohyundev.review.employee.domain.dto
 
-data class RegisterEmployeeCommand()
+import com.dohyundev.review.employee.domain.entity.EmployeeRole
+import java.time.LocalDate
+
+data class RegisterEmployeeCommand(
+    val no: String,
+    val name: String,
+    val hireDate: LocalDate,
+    val role: EmployeeRole = EmployeeRole.USER,
+)
--- src/main/kotlin/com/dohyundev/review/employee/domain/dto/UpdateEmployeeCommand.kt
+++ src/main/kotlin/com/dohyundev/review/employee/domain/dto/UpdateEmployeeCommand.kt
@@ -1,4 +1,12 @@
 package com.dohyundev.review.employee.domain.dto
 
-class UpdateEmployeeCommand {
-}
+import com.dohyundev.review.employee.domain.entity.EmployeeRole
+import kr.co.modaoutlet.mgr.employee.domain.EmployeeStatus
+import java.time.LocalDate
+
+data class UpdateEmployeeCommand(
+    val name: String,
+    val role: EmployeeRole,
+    val hireDate: LocalDate,
+    val status: EmployeeStatus,
+)
