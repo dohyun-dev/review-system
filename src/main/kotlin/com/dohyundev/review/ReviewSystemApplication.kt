73b9c598b12efc77eb603616be7b9740df320008
dohyundev <dohyundev0425@gmail.com>
2026-03-25 19:16:17 +0900

    first commit

---
+++ src/main/kotlin/com/dohyundev/review/ReviewSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.dohyundev.review
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReviewSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReviewSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/dohyundev/review/ReviewSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dohyundev.review
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReviewSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
